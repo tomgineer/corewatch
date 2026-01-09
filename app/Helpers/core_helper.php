17513221f06cec8514087d0fa0e1fbc15bb07aa8
Tom Papatolis <49147550+tompapatolis@users.noreply.github.com>
2026-01-09 19:15:27 +0100

    Number Formatting From Browser

--- app/Helpers/core_helper.php
+++ app/Helpers/core_helper.php
@@ -2,7 +2,10 @@
 
 if (!function_exists('formatNumber')) {
     function formatNumber(float $number, int $maxDecimals = 2): string {
-        $locale = service('request')->getLocale();
+        $acceptLanguage = service('request')->getHeaderLine('Accept-Language');
+        $locale = \Locale::acceptFromHttp($acceptLanguage) ?: 'en_US';
+
+        $locale = str_replace('-', '_', $locale);
 
         $fmt = new \NumberFormatter($locale, \NumberFormatter::DECIMAL);
         $fmt->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $maxDecimals);
